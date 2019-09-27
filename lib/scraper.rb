a02052f1caf77ddb6067de7c04260e3f1b2e463a
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-27 19:22:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 
-puts doc.css(".headline-26OIBN").strip
+# puts doc.css(".headline-26OIBN").strip
 
 puts doc.css(".headline-26OIBN").text
 
